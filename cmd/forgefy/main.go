614446a459a2e647904165661bf91a86967af14c
huszkacs <huszkacs@gmail.com>
2024-04-20 16:05:55 +0800

    Implements cli flags to specifying where the forge file is and output directory of the forged product

--- cmd/forgefy/main.go
+++ cmd/forgefy/main.go
@@ -1,47 +1,58 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/followthepattern/forgefy"
 	"github.com/followthepattern/forgefy/io"
+	"github.com/spf13/cobra"
 )
 
 func main() {
-	yaml := `
-version: 0
-product_name: "test product"
-apps:
-  - name: backend1
-    type: backend
-  - name: frontend1
-    type: frontend
-features:
-  - name: user
-    fields:
-      - name: ID
-        type: string
-      - name: email
-        type: string
-      - name: FirstName
-        type: string
-      - name: LastName
-        type: string
-      - name: Age
-        type: int
-  - name: product
-    fields:
-      - name: ID
-        type: string
-      - name: Name
-        type: string
-      - name: Type
-        type: string
-      - name: Price
-        type: int
-  - name: task
-  - name: image
-`
-	f := forgefy.New()
-	fw := io.NewFileWriter("output")
+	var file string
+	var outputDir string
 
-	f.Forge(yaml, fw)
+	var root = &cobra.Command{
+		Use:   "forgefy",
+		Short: "forgefy generates web applications from a simple config.",
+		Long:  `forgefy generates web applications using golang template framework from a forge config file, that uses yaml syntax.`,
+		Run: func(cmd *cobra.Command, args []string) {
+			if file == "" {
+				fmt.Fprintln(os.Stdout, "file can't be empty")
+				cmd.Help()
+				os.Exit(1)
+			}
+			if outputDir == "" {
+				fmt.Fprintln(os.Stdout, "output can't be empty")
+				cmd.Help()
+				os.Exit(1)
+			}
+		},
+	}
+
+	root.Flags().StringVarP(&file, "file", "f", "", "filepath to the forge file")
+	root.Flags().StringVarP(&outputDir, "output", "o", "", "output directory path")
+
+	if err := root.Execute(); err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	forgeFile, err := os.ReadFile(file)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	f := forgefy.New()
+	fw := io.NewFileWriter(outputDir)
+
+	productName, err := f.Forge(string(forgeFile), fw)
+	if err != nil {
+		fmt.Printf("during forging %s following error occured: %s", productName, err.Error())
+		os.Exit(1)
+	}
+
+	fmt.Printf("%s is successfully forged to %s", productName, outputDir)
 }
