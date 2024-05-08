bb48a17af31fd7adc7071e447b68a62edcc6ba05
huszkacs <huszkacs@gmail.com>
2024-05-08 14:36:16 +0800

    Refactor walk function

--- plugins/gobackend/app.go
+++ plugins/gobackend/app.go
@@ -7,9 +7,6 @@ import (
 
 	"github.com/followthepattern/forgefy/plugins"
 	"github.com/followthepattern/forgefy/plugins/gobackend/apptemplates"
-	"github.com/followthepattern/forgefy/plugins/gobackend/apptemplates/models"
-	"github.com/followthepattern/forgefy/plugins/gobackend/apptemplates/tests/integration/testdata"
-	"github.com/followthepattern/forgefy/plugins/gobackend/apptemplates/types"
 	"github.com/followthepattern/forgefy/productmap"
 	"github.com/followthepattern/forgefy/specification"
 )
@@ -26,45 +23,17 @@ func (GoBackendPluginApp) Type() string {
 	return "go-backend"
 }
 
-func (GoBackendPluginApp) AddStaticFiles(pm productmap.ProductMap, fs specification.Product, app specification.App) error {
-	if err := pm.Insert(types.Directory(app.AppName),
-		types.Files...); err != nil {
-		return err
-	}
-
-	if err := pm.Insert(models.Directory(app.AppName),
-		models.Files...); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func (plugin GoBackendPluginApp) Builder2(pm productmap.ProductMap, fs specification.Product, app specification.App) error {
-	dir := apptemplates.Directory(app.AppName)
-
-	features := append(fs.Features, app.Features...)
-
-	err := plugin.addTests(pm, app, features)
-	if err != nil {
-		return err
-	}
-
-	err = plugin.AddStaticFiles(pm, fs, app)
-	if err != nil {
-		return err
-	}
-
-	return pm.Insert(dir,
-		apptemplates.GoMod.WithData(app),
-		apptemplates.DockerFile.WithData(app))
-}
-
 func (plugin GoBackendPluginApp) Builder(pm productmap.ProductMap, product specification.Product, app specification.App) error {
+	dir := apptemplates.EntireDir
+
+	return fs.WalkDir(dir, ".", plugin.createWalkFn(pm, product, app))
+}
+
+func (b GoBackendPluginApp) createWalkFn(pm productmap.ProductMap, product specification.Product, app specification.App) func(filepath string, d fs.DirEntry, err error) error {
 	dir := apptemplates.EntireDir
 	features := append(product.Features, app.Features...)
 
-	return fs.WalkDir(dir, ".", func(filepath string, d fs.DirEntry, err error) error {
+	return func(filepath string, d fs.DirEntry, err error) error {
 		if err != nil {
 			return err
 		}
@@ -80,31 +49,9 @@ func (plugin GoBackendPluginApp) Builder(pm productmap.ProductMap, product speci
 
 		filepath = strings.TrimSuffix(filepath, ".tmpl")
 
-		if strings.Contains(filepath, "[feature]") {
-			filepath = path.Join(apptemplates.Directory(app.AppName), filepath)
+		filepath = path.Join(apptemplates.Directory(app.AppName), filepath)
 
-			for _, feature := range features {
-				feat := Feature(feature)
-
-				newFilePath := strings.ReplaceAll(filepath, "[feature]", feat.PackageName())
-
-				dirName, fileName := path.Split(newFilePath)
-				file := productmap.NewFile(
-					fileName,
-					string(content),
-				).WithData(FeatureTemplateModel{
-					Feature: feat,
-					App:     app,
-				})
-
-				err = pm.Insert(dirName, file)
-				if err != nil {
-					return err
-				}
-			}
-		} else {
-			filepath = path.Join(apptemplates.Directory(app.AppName), filepath)
-
+		if !strings.Contains(filepath, "[feature]") {
 			dirName, fileName := path.Split(filepath)
 			file := productmap.NewFile(
 				fileName,
@@ -113,26 +60,27 @@ func (plugin GoBackendPluginApp) Builder(pm productmap.ProductMap, product speci
 
 			return pm.Insert(dirName, file)
 		}
+
+		for _, feature := range features {
+			feat := Feature(feature)
+
+			newFilePath := strings.ReplaceAll(filepath, "[feature]", feat.PackageName())
+
+			dirName, fileName := path.Split(newFilePath)
+			file := productmap.NewFile(
+				fileName,
+				string(content),
+			).WithData(FeatureTemplateModel{
+				Feature: feat,
+				App:     app,
+			})
+
+			err = pm.Insert(dirName, file)
+			if err != nil {
+				return err
+			}
+		}
+
 		return nil
-	})
-}
-
-func (b GoBackendPluginApp) addTests(pm productmap.ProductMap, app specification.App, features []specification.Feature) error {
-	dir := testdata.Directory(app.AppName)
-
-	d := struct {
-		Features []specification.Feature
-		specification.App
-	}{
-		Features: features,
-		App:      app,
 	}
-
-	for i := 0; i < len(testdata.Files); i++ {
-		testdata.Files[i] = testdata.Files[i].WithData(d)
-	}
-
-	return pm.Insert(dir,
-		testdata.Files...,
-	)
 }
