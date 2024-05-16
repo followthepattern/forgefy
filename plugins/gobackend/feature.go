60dd7c04217529c559d4aee7ced3fd5767b374d7
huszkacs <huszkacs@gmail.com>
2024-05-16 14:58:47 +0800

    Handles different types for golang

--- plugins/gobackend/feature.go
+++ plugins/gobackend/feature.go
@@ -21,3 +21,21 @@ func (f Feature) PackageName() string {
 func (f Feature) FeatureTableName() string {
 	return strings.ToLower(f.FeatureName)
 }
+
+func (f Feature) Fields() []specification.Field {
+	fields := f.Feature.Fields
+	for i := range fields {
+		fields[i].Type = f.overrideFields(fields[i].Type)
+	}
+
+	return fields
+}
+
+func (f Feature) overrideFields(fieldType string) string {
+	switch fieldType {
+	case "string":
+		return "types.String"
+	default:
+		return fieldType
+	}
+}
