578211e75e672cc35388ddfb7694465c3c61a891
huszkacs <huszkacs@gmail.com>
2025-09-24 10:42:09 +0200

    Implements flutter plugin

---
+++ plugins/flutter/plugin.go
@@ -0,0 +1,21 @@
+package flutter
+
+import (
+	"github.com/followthepattern/forgefy/plugins"
+	"github.com/followthepattern/forgefy/productmap"
+	"github.com/followthepattern/forgefy/specification"
+)
+
+var _ plugins.Plugin = Plugin{}
+
+type Plugin struct{}
+
+func (Plugin) Name() string { return "Go Plugin" }
+
+func (Plugin) Apps() []plugins.App {
+	return []plugins.App{
+		NewApp(),
+	}
+}
+
+func (Plugin) Build(_ productmap.ProductMap, _ specification.Product) error { return nil }
